2b74ef7e9bcedbbe8aaba6db14d98941fe372901
Phoenicis <alliefirebaugh@gmail.com>
2019-02-21 02:57:16 -0500

    added some fake friends to start out the API

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,118 @@
 // You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-// ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
-// ```
+var friendData = [
+    {
+        name: "Stewie Griffin",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Stewie_Griffin.png/220px-Stewie_Griffin.png",
+        scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    },
+    {
+        name: "Peter Griffin",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/Peter_Griffin.png/220px-Peter_Griffin.png",
+        scores:[
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+        ]
+    },
+    {
+        name: "Lois Griffin",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a5/Lois_Griffin.png/170px-Lois_Griffin.png",
+        scores:[
+        5,
+        5,
+        4,
+        4,
+        5,
+        5,
+        5,
+        5,
+        4,
+        5
+        ]
+    },
+    {
+        name: "Glenn Guagmire",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/fe/Glenn_Quagmire.png/220px-Glenn_Quagmire.png",
+        scores:[
+        2,
+        1,
+        1,
+        2,
+        1,
+        1,
+        2,
+        2,
+        3,
+        1
+        ]
+    },
+    {
+        name: "Cleveland Brown",
+        photo: "https://upload.wikimedia.org/wikipedia/en/e/e4/Cleveland_Brown.png",
+        scores:[
+        4,
+        2,
+        4,
+        4,
+        2,
+        2,
+        2,
+        5,
+        4,
+        2
+        ]
+    },
+    {
+        name: "Joe Swanson",
+        photo: "https://vignette.wikia.nocookie.net/familyguy/images/9/9c/190px-Joe_Swanson.png/revision/latest?cb=20100326012234",
+        scores:[
+        5,
+        1,
+        4,
+        3,
+        5,
+        1,
+        2,
+        3,
+        4,
+        1
+        ]
+    },
+    {
+        name: "Meg Griffin",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Meg_Griffin.png/185px-Meg_Griffin.png",
+        scores:[
+        5,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1,
+        5,
+        1,
+        1
+        ]
+    }
+]
+
+module.exports = friendData;
