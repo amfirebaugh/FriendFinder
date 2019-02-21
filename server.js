f28216733acd1d32e3211cb96690fc012532490e
Phoenicis <alliefirebaugh@gmail.com>
2019-02-21 04:04:19 -0500

    functionality updates to paths and jQuery debugging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // dependencies
 var express = require("express");
-//var path = require("path");
+var path = require("path");
+var bodyParser = require("body-parser");
 
 // create express app
 var app = express();
@@ -10,9 +11,12 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // require the routing files
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 // server listening
 app.listen(PORT, function() {
