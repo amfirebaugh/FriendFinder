f65f5b1322345b59559c30e84ec821b082d3dc78
Phoenicis <alliefirebaugh@gmail.com>
2019-02-21 14:41:28 -0500

    added best match calulations and continuing to work debugging issues

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,5 +12,20 @@ module.exports = function(app) {
     // API POST request
     app.post("/api/friends", function(req, res) {
         friendData.push(req.body);
+        var userScoresArray = req.body.scores;
+        var calcOneQ = 0;
+        var calcTotal = [];
+
+        for (var i = 0; i < friendData.length - 1; i++) {
+            for (var j = 0; j < userScoresArray.length; j++) {
+                calcOneQ += (Math.abs(parseInt(friendData[i].scores[j]) - parseInt(userScoresArray[j])));
+            }
+            calcTotal.push(calcOneQ);
+        }
+        var calcMatch = Math.min(...calcTotal);
+        var matchNum = calcTotal.indexOf(calcMatch);
+        var match = friendData[matchNum];
+
+        res.json(match);
     });
 };
